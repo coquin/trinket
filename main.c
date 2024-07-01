b79a9a695d0359cb8118fe2de623af6f4e40c6fb
Denis Kokin <denis.kokin@gmail.com>
2024-07-01 09:19:13 +0200

    Using bit constants and bit masks
    Found example here https://embeddedthoughts.com/2016/05/25/getting-started-with-the-attiny85/

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 int main (void)
 {
     // Set Data Direction to output on port B, pins 1:
-    DDRB = 0b00000010;
+    DDRB |= (1 << PB1);
     while (1) {
         // set PB1 high
-        PORTB = 0b00000010;
+        PORTB |= (1 << PB1);
         _delay_ms(200);
         // set PB1 low
-        PORTB = 0b00000000;
+        PORTB &= ~(1 << PB1);
         _delay_ms(1000);
     }
 
